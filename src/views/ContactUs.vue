e020d4108fd7f36630bf9e2d6fc1d11b7bfe0247
Gandobilis <lashadeveloper@gmail.com>
2023-06-07 01:00:07 +0400

    ContactUs.vue final version.

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,18 +1,11 @@
 <template>
-  <section class="bg-[#6967CE] h-[max(185px,48.7vh)]">
-    <div
-      class="flex flex-col gap-y-[2.8vh] max-sm:gap-y-[27px] w-[max(350px,49.4vw)] ml-[25.4vw] pt-[max(40px,11.8vh)] max-sm:text-center max-sm:mx-auto"
-    >
-      <h1
-        class="text-white text-[max(36px,3.8vw)] font-medium max-sm:font-bold leading-[max(48px,4.3vw)]"
-      >
-        Contact Us
+  <section :class="contactUsCss.section">
+    <div :class="contactUsCss.container">
+      <h1 :class="contactUsCss.h1">
+        {{ contactUsData.h1 }}
       </h1>
-      <p
-        class="text-[#BDC8DB] text-[max(16px,1.1vw)] font-normal leading-[max(24px,2.1vw)] tracking-[-0.5px]"
-      >
-        Whether you're needing help with a new project or just want to pick our
-        brain we're here for you!
+      <p :class="contactUsCss.p">
+        {{ contactUsData.p }}
       </p>
     </div>
   </section>
@@ -21,4 +14,16 @@
 
 <script setup>
 import Contact from "../components/Contact.vue";
+
+const contactUsData = {
+  h1: "Contact Us",
+  p: "Whether you're needing help with a new project or just want to pick our brain we're here for you!",
+};
+const contactUsCss = {
+  section: "bg-[#6967CE]",
+  container:
+    "flex flex-col pt-[max(40px,4.69vw)] pb-[max(40px,3.91vw)] gap-y-[1.04vw] max-sm:gap-7 w-[max(350px,39.58vw)] ml-[max(20px,20.31vw)] max-sm:text-center max-sm:mx-auto",
+  h1: "text-white text-[max(36px,3.07vw)] font-medium max-sm:font-bold leading-[max(48px,3.44vw)] ml-4 max-sm:ml-0",
+  p: "text-[#BDC8DB] text-[max(16px,1.15vw)] font-normal leading-[max(24px,1.61vw)] tracking-[-0.5px] ml-4 max-sm:ml-0",
+};
 </script>
