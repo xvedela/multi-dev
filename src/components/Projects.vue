4b41186f10fca88ef27943b67a20e3629f8d1d02
Gandobilis <lashadeveloper@gmail.com>
2023-06-28 11:22:18 +0400

    Reviews view and composable final version.

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,7 +4,7 @@
         class="w-[90wv] sm:max-2xl:w-[75vw] 2xl:w-[60vw] flex flex-col items-center gap-y-10 pt-7 sm:max-2xl:pt-16 pb-20 2xl:py-24 mx-5 sm:mx-auto">
       <h2 class="font-medium text-2xl sm:max-2xl:text-3xl 2xl:text-4xl max-2xl:text-center" v-text="'Recent Projects'"/>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-x-5 gap-y-5">
-        <img v-for="(project, index) in projects" :key="index" src="../assets/projects/cart-rescue.png"
+        <img v-for="(project, index) in projects" :key="index" :src="project.projectImage"
              alt="project image"/>
       </div>
       <router-link :to="{name: 'OurWork'}"
@@ -18,6 +18,9 @@
 
 <script setup>
 import useProjects from "../composables/components/useProjects.js";
+import {onMounted} from "vue";
 
-const projects = useProjects();
+let projects;
+
+onMounted(async () => projects = await useProjects());
 </script>
--- src/composables/components/useReviews.js
+++ src/composables/components/useReviews.js
@@ -1,16 +1,15 @@
 export default function useReviews() {
-  const response = {
-    "data": Array(3).fill({
-      "authorImage": "https://icepick.co/wp-content/uploads/2022/08/1661371646793-300x300.jpeg",
-      "authorName": "Jennifer Hall",
-      "companyName": "AegisLiving",
-      "websiteUrl": "https://www.aegisliving.com/",
-      "reviewText": `Icepick was able to help us get a very difficult web redesign
+  const data = Array(3).fill({
+    "authorImage": "https://icepick.co/wp-content/uploads/2022/08/1661371646793-300x300.jpeg",
+    "authorName": "Jennifer Hall",
+    "companyName": "AegisLiving",
+    "websiteUrl": "https://www.aegisliving.com/",
+    "reviewText": `Icepick was able to help us get a very difficult web redesign
 project across the finish line and continues to manage a large list
 of projects to help improve user experience and SEO. The team is
 knowledgeable, highly skilled and very responsive.`,
-    }),
-  };
+  });
+  const response = {data};
 
   return new Promise((resolve, reject) => {
     const data = response.data;
